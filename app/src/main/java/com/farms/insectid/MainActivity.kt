c1434e4ed8e89201c73ede4bd8cc21129860e741
oops-creator <still.shivam@gmail.com>
2020-04-22 05:36:15 +0530

    Add image picker

--- app/src/main/java/com/farms/insectid/MainActivity.kt
+++ app/src/main/java/com/farms/insectid/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.farms.insectid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.ResolveInfo
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.insect_image).setOnClickListener {
+
+            val OpenImage = Intent(Intent.ACTION_GET_CONTENT)
+            OpenImage.type = "*/*"
+
+
+            val activities: List<ResolveInfo> = packageManager.queryIntentActivities(OpenImage, 0)
+            val isIntentSafe: Boolean = activities.isNotEmpty()
+
+
+            if (isIntentSafe) {
+                startActivityForResult(OpenImage, 0)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != RESULT_OK) {
+            return;
+        } else {
+            val retUri: Uri? = data?.data
+           
+           
+
+
+        }
+
+
+
     }
 }
