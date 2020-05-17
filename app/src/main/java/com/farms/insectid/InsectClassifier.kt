31899df5326f66fb5d12869b1f448e43045784b8
oops-creator <still.shivam@gmail.com>
2020-05-17 16:39:48 +0530

    Add classifier class to load model and classify image

---
+++ app/src/main/java/com/farms/insectid/InsectClassifier.kt
@@ -0,0 +1,126 @@
+package com.farms.insectid
+
+import android.content.Context
+import android.content.res.AssetFileDescriptor
+import android.content.res.AssetManager
+import android.graphics.Bitmap
+import android.util.Log
+import org.tensorflow.lite.Interpreter
+import java.io.FileInputStream
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.channels.FileChannel
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class InsectClassifier(private val context: Context) {
+
+    private var interpreter: Interpreter? = null
+    var isInitialized = false
+        private set
+
+    companion object {
+        private const val TAG = "DigitClassifier"
+
+        private const val FLOAT_TYPE_SIZE = 4
+        private const val PIXEL_SIZE = 3
+
+        private const val OUTPUT_CLASSES_COUNT = 4
+    }
+
+
+    // Executor to do inference
+    private val executorService: ExecutorService = Executors.newCachedThreadPool()
+
+    private var inputImageWidth: Int = 0
+    private var inputImageHeight: Int = 0
+    private var modelInputSize: Int = 0
+
+    @Throws(IOException::class)
+    private fun initializeInterpreter() {
+
+
+        // Load the TF Lite model from asset folder
+        val assetManager = context.assets
+        val model = loadModelFile(assetManager, "modelInsect.tflite")
+        val options = Interpreter.Options()
+        options.setUseNNAPI(true)
+        val interpreter = Interpreter(model, options)
+
+        // Read input shape from model file.
+        val inputShape = interpreter.getInputTensor(0).shape()
+        inputImageWidth = inputShape[1]
+        inputImageHeight = inputShape[2]
+        modelInputSize = FLOAT_TYPE_SIZE * inputImageWidth *
+                inputImageHeight * PIXEL_SIZE
+
+        // Finish interpreter initialization.
+        this.interpreter = interpreter
+
+        isInitialized = true
+        Log.d(TAG, "Initialized interpreter.")
+    }
+
+
+    @Throws(IOException::class)
+    private  fun loadModelFile(assetManager: AssetManager , filename: String):ByteBuffer{
+        val fileDescriptor = assetManager.openFd(filename)
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel = inputStream.channel
+        val startOffset = fileDescriptor.startOffset
+        val declaredLength = fileDescriptor.declaredLength
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY , startOffset , declaredLength)
+        
+    }
+
+    private fun classify(bitmap: Bitmap): String {
+        check(isInitialized) { "TF Lite not initialized" }
+
+  
+        val resizedImage = Bitmap.createScaledBitmap(
+            bitmap,
+            inputImageWidth,
+            inputImageHeight,
+            true
+        )
+        val byteBuffer = convertBitmapToByteBuffer(resizedImage)
+
+        //Array to store the model output.
+        val output = Array(1) { FloatArray(OUTPUT_CLASSES_COUNT) }
+
+        // Run inference
+        interpreter?.run(byteBuffer, output)
+
+
+
+        val result = output[0]
+        val maxIndex = result.indices.maxBy { result[it] } ?: -1
+        val resultString =
+            "Prediction Result: %d\nConfidence: %2f"
+                .format(maxIndex, result[maxIndex])
+
+        Log.e("result" , resultString)
+        return resultString
+    }
+
+    private fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
+        val byteBuffer = ByteBuffer.allocateDirect(modelInputSize)
+        byteBuffer.order(ByteOrder.nativeOrder())
+
+        val pixels = IntArray(inputImageWidth * inputImageHeight)
+        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+
+
+        for (pixelValue in pixels) {
+
+            // Normalize pixel value to 0 to 1
+            val normalizedPixelValue = pixelValue/ 255.0f
+
+            byteBuffer.putFloat(normalizedPixelValue)
+        }
+
+        return byteBuffer
+    }
+
+}
